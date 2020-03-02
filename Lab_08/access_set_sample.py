98c8c89f4745df7657dc7339bc206d1d5c23353f
vptuan <vptuan@users.noreply.github.com>
2020-03-02 22:32:46 +0000

    Create access_set_sample.py

---
+++ Lab_08/access_set_sample.py
@@ -0,0 +1,14 @@
+# Creating a set 
+set1 = set(["COMP", "1819", "ADS"]) 
+print("\nInitial set") 
+print(set1) 
+  
+# Accessing element using 
+# for loop 
+print("\nElements of set: ") 
+for i in set1: 
+    print(i, end=" ") 
+  
+# Checking the element 
+# using in keyword 
+print("Python" in set1)
