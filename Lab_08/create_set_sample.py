2796ddff49b99bed90a474dff5b1c9056d6a5327
vptuan <vptuan@users.noreply.github.com>
2020-03-02 22:25:29 +0000

    Create create_set_sample.py

---
+++ Lab_08/create_set_sample.py
@@ -0,0 +1,16 @@
+# Creating a Set 
+set1 = set() 
+print("Intial blank Set: ") 
+print(set1) 
+  
+# Creating a Set with  
+# the use of a String 
+set1 = set("COMP1819ADS") 
+print("\nSet with the use of String: ") 
+print(set1) 
+  
+# Creating a Set with 
+# the use of a List 
+set1 = set(["COMP", "1819", "ADS"]) 
+print("\nSet with the use of List: ") 
+print(set1) 
